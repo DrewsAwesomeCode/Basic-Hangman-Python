ce61cb94047c0b2e3081eb53fe38d5b45ddcfc8b
Drew <ayersdg@g.cofc.edu>
2019-01-13 21:47:44 -0500

    Hangman Python Program
    VERY simple and basic hangman game created for a introductory programming course. Written in Python

---
+++ Hangman.py
@@ -0,0 +1,56 @@
+## Hangman.py
+## Name: Andrew Ayers
+## April 10th, 2017
+
+import random
+
+def openWords(infile):
+    file = open(infile, "r")
+    words = []
+    for line in file:
+        lower = line.lower()
+        word = lower.split()
+        words += word[0:]
+    return words
+
+def randomPick(words):
+    secretWord = random.choice(words)
+    return secretWord
+
+def underscore(secretWord):
+    underscores = []
+    for i in range(len(secretWord)):
+        underscores += "-"
+    return underscores
+
+def guess(guess):
+    guess = input("Guess a letter - ")
+    guess = guess.lower()
+    if ord(guess) >= 97 and ord(guess) <= 122:
+        letters += guess
+        return letters
+    else:
+        print("Not a valid entry!")
+
+def guessed(letters, secretWord):
+    
+
+##def spelled(letters, secretWord):
+##    if 
+##    
+
+##def playGame():
+##    print("Welcome to Hangman!")
+##    words = openWords("wordlist.txt")
+##    secretWord = randomPick(words)
+##    print(secretWord)
+##    underscores = underscore(secretWord)
+##    print("Your word =  ", end = "")
+##    for item in underscores:
+##        print(item, end = " ")
+##    print()
+
+    
+    
+   
+playGame()
